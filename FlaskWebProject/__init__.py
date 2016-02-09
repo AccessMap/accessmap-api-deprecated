c48751fba732950d6b5f7f4a4e6c5a50cf8f01bf
Nick Bolten <nbolten@gmail.com>
2016-02-09 11:39:26 -0800

    Added sqlalchemy

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,8 +1,13 @@
-"""
-The flask application package.
-"""
-
+'''The flask application package.'''
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry
+import os
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy()
+
 
 import FlaskWebProject.views
