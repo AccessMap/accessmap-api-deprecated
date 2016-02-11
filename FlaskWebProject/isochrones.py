12083c25c5a9f2f07dc907233a41a4789abb06be
Nick Bolten <nbolten@gmail.com>
2016-02-11 03:23:59 -0800

    Re-added scipy to see if it is the deployment issue

--- FlaskWebProject/isochrones.py
+++ FlaskWebProject/isochrones.py
@@ -3,7 +3,7 @@ import geojson
 import json
 # import matplotlib.pyplot as plt
 import numpy as np
-# from scipy.interpolate import griddata
+from scipy.interpolate import griddata
 
 
 def get_isochrone(lonlat, costcol='cost'):
