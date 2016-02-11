69faf5bd7355a006fc2186e7b4329e1ad4a8ffcb
Nick Bolten <nbolten@gmail.com>
2016-02-11 14:23:34 -0800

    Removed CORS temporarily

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -9,4 +9,15 @@ app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 
+# CORS responses
+# FIXME: re-enable CORS soon
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers",
+                         "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET")
+
+    return response
+
 import FlaskWebProject.views
