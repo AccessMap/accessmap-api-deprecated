5ec3bd9c05009d0e9953308d8c3b697ded455d76
= <=>
2018-08-20 17:30:13 -0700

    Update default test location

--- tests/profile.py
+++ tests/profile.py
@@ -3,7 +3,7 @@ from accessmapapi.routing import costs, route
 
 
 origin = Point([-122.32508034811278, 47.645040425347645])
-destination = Point([-122.3328839, 47.6064165])
+destination = Point([-122.279865, 47.676246])
 
 route_response = route.dijkstra(origin, destination,
                                 cost_fun_gen=costs.cost_fun_generator)
